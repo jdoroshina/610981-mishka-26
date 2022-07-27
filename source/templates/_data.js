0477a999696cf3cafcc46271dda8ea89b2b22689
firefoxic <firefoxic.dev@gmail.com>
2022-07-27 09:19:37 +0300

    Set up project automatization

---
+++ source/templates/_data.js
@@ -0,0 +1,31 @@
+export default {
+	"pages": [
+		{
+			"path": "index.html",
+			"name": "Главная"
+		},
+		{
+			"path": "catalog.html",
+			"name": "Каталог товаров"
+		},
+		{
+			"path": "form.html",
+			"name": "Вязание на заказ"
+		}
+	],
+	"viewports": {
+		"desktop": 1150,
+		"tablet": 768,
+		"mobile": null
+	},
+	"images": {
+		"map": {
+			"ext": "png",
+			"width": 320,
+			"height": 454,
+			"maxdppx": 2,
+			"alt": "Магазин по адресу: улица Большая Конюшенная дом 19/8 офис 101, город Санкт-Петербург.",
+			"viewports": ["desktop", "tablet", "mobile"]
+		}
+	}
+}
