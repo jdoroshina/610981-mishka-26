313d8c9d5326ae9f6b339c5237ff30f3eec8f5cf
jdoroshina <36820104+jdoroshina@users.noreply.github.com>
2022-09-16 10:52:05 +0000

    Deploying to gh-pages from @ jdoroshina/610981-mishka-26@dede9a2e5f6b55f1ea118a08464c0075c541cab4 🚀

---
+++ js/nav-toggle.js
@@ -0,0 +1,1 @@
+let nav=document.querySelector(".navigation"),navToggle=nav.querySelector(".navigation__toggle");nav.classList.remove("navigation--js-none"),nav.classList.add("navigation--is-closed"),navToggle.addEventListener("click",(()=>{nav.classList.toggle("navigation--is-closed")}));
